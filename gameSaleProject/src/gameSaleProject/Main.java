93f3c5b8261e172d5828d647933f29b4cd8bda1e
TugceOnder <tugceonder775@gmail.com>
2021-05-12 16:11:31 +0300

    Update Main.java

--- gameSaleProject/src/gameSaleProject/Main.java
+++ gameSaleProject/src/gameSaleProject/Main.java
@@ -16,22 +16,22 @@ public class Main {
 	public static void main(String[] args) {
 		
 		GamerManager gamerManager1 = new GamerManager(new MernisServiceAdapter());
-		gamerManager1.add(new Gamer(1, "Tuðba", "Çýrakoðullarý",LocalDate.of(1990,1,21), "1111111111"));
+		gamerManager1.add(new Gamer(1, "Tugce", "Onder",LocalDate.of(1996,2,1), "1111111111"));
 		
 		GamerManager gamerManager2 = new GamerManager(new MernisServiceAdapter());
-		gamerManager2.add(new Gamer(2, "Yýldýray", "Çýrakoðullarý",LocalDate.of(1991,5,30), "22222222222"));
+		gamerManager2.add(new Gamer(2, "Tuna", "Onder",LocalDate.of(1986,3,30), "22222222222"));
 		
 		GameManager gameManager = new GameManager();
 		gameManager.add(new Game(1,"Need For Speed-Most Wanted",1000,5));
 		
 		CampaignManager campaignManager = new CampaignManager();
-		campaignManager.add(new Campaign(1, "Yýlbaþý", 10));
+		campaignManager.add(new Campaign(1, "YÄ±lbaÅŸÄ±", 10));
 		
 		SaleManager saleManager1 = new SaleManager();
-		saleManager1.sale(new Game(1,"Need For Speed-Most Wanted",1000,5),new Gamer(1, "Tuðba", "Çýrakoðullarý",LocalDate.of(1990,1,21), "1111111111"));
+		saleManager1.sale(new Game(1,"Need For Speed-Most Wanted",1000,5),new Gamer(1, "Tugce", "Onder",LocalDate.of(1996,2,1)), "1111111111"));
 		
 		SaleManager saleManager2 = new SaleManager();
-		saleManager2.campaignSale(new Game(1,"Need For Speed-Most Wanted",1000,5),new Gamer(2, "Yýldýray", "Çýrakoðullarý",LocalDate.of(1991,5,30), "22222222222"),new Campaign(1, "Yýlbaþý", 10));
+		saleManager2.campaignSale(new Game(1,"Need For Speed-Most Wanted",1000,5),new Gamer(2, "Tuna", "Onder",LocalDate.of(1986,3,30), "22222222222"),new Campaign(1, "YÄ±lbaÅŸÄ±", 10));
 		
 
 }
